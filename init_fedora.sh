59da58f6bcfd57c2570d7ab9007556e63eea249d
Jonas Richter <richter-jonas@outlook.com>
2023-10-26 13:32:14 +0200

    update

--- init_fedora.sh
+++ init_fedora.sh
@@ -10,6 +10,28 @@ run_as_root() {
     echo $PASSWORD | sudo -S $@
 }
 
+get_user_response() {
+    local prompt_message="$1"
+    local user_response
+
+    while true; do
+        echo "$prompt_message (y/n)"
+        read user_response
+        case $user_response in
+            [Yy]* ) user_response='y'; break;;  # Yes
+            [Nn]* ) user_response='n'; break;;  # No
+            * ) echo "Please answer 'y' for yes or 'n' for no.";;
+        esac
+    done
+
+    echo $user_response  # This will return the value to the caller.
+}
+
+install_vscode_extensions=$(get_user_response "Do you wish to install VS Code Extensions?")
+install_python_extension=$(get_user_response "VS Code Extensions: Install Python extensions?")
+install_ssh_extension=$(get_user_response "VS Code Extensions: Install SSH extensions?")
+install_java_extension=$(get_user_response "VS Code Extensions: Install Java extensions?")
+
 # Ask the user if they want to set up Git.
 echo "Do you wish to set up global Git configuration? (yes/no)"
 read SETUP_GIT
@@ -124,6 +146,11 @@ else
     echo "No changes made to the display server settings."
 fi
 
+
+
+
+
+
 # Update and upgrade the system
 run_as_root dnf update -y
 run_as_root dnf upgrade -y
@@ -143,11 +170,8 @@ run_as_root dnf -y install ffmpeg-devel
 
 run_as_root dnf install @development-tools
 
-# Install VS Code
-run_as_root rpm --import https://packages.microsoft.com/keys/microsoft.asc
-run_as_root sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-run_as_root dnf check-update
-run_as_root dnf install -y code
+
+
 
 
 # Install Docker
@@ -176,16 +200,87 @@ run_as_root usermod -aG docker $USER
 run_as_root newgrp docker
 
 
+
+
+
+
+# Install VS Code
+run_as_root rpm --import https://packages.microsoft.com/keys/microsoft.asc
+run_as_root sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+run_as_root dnf check-update
+run_as_root dnf install -y code
+
+declare -a PYTHON_EXTENSIONS=(
+    "ms-python.python"
+    "ms-python.vscode-pylance"
+)
+declare -a SSH_EXTENSIONS=(
+    "ms-vscode-remote.remote-ssh"
+)
+declare -a DOCKER_EXTENSIONS=(
+    "ms-azuretools.vscode-docker"
+)
+declare -a JAVA_EXTENSIONS=(
+    "vscjava.vscode-java-pack"
+)
+
+if [ "$install_vscode_extensions" = "y" ]; then
+    echo "User chose to install VS Code extensions. Proceeding..."
+
+    if [ "$install_python_extension" = "y" ]; then
+        echo "Installing Python extensions..."
+        for extension in "${PYTHON_EXTENSIONS[@]}"; do
+            code --install-extension $extension
+        done
+    fi
+
+    if [ "$install_python_extension" = "y" ]; then
+        echo "Installing Python extensions..."
+        for extension in "${SSH_EXTENSIONS[@]}"; do
+            code --install-extension $extension
+        done
+    fi
+
+    if [ "$install_python_extension" = "y" ]; then
+        echo "Installing Python extensions..."
+        for extension in "${DOCKER_EXTENSIONS[@]}"; do
+            code --install-extension $extension
+        done
+    fi
+    
+    if [ "$install_python_extension" = "y" ]; then
+        echo "Installing Python extensions..."
+        for extension in "${JAVA_EXTENSIONS[@]}"; do
+            code --install-extension $extension
+        done
+    fi
+else
+    echo "User chose not to install any VS Code extensions."
+fi
+
+
+
+
+
 # Set DNF metadata expiry to 7 days for quick package installation
 grep -q "^#metadata_expire" /etc/dnf/dnf.conf && echo $PASSWORD | sudo -S sed -i 's/^#metadata_expire.*/metadata_expire=7d/' /etc/dnf/dnf.conf || \
 grep -q "^metadata_expire" /etc/dnf/dnf.conf && echo $PASSWORD | sudo -S sed -i 's/^metadata_expire.*/metadata_expire=7d/' /etc/dnf/dnf.conf || \
 echo "metadata_expire=7d" | sudo tee -a /etc/dnf/dnf.conf
 run_as_root dnf clean all
 
+
+
+
 # Enable minimize and maximize buttons on window title bars in GNOME.
 gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
 
+
+
+
 # Clear the password variable at the end of the script
 unset PASSWORD
+
+
+
 
 echo "Initialization completed."
